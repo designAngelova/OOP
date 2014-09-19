f5ae12b06e743db2e353a1e7fcf007b09b7023cc
Team-Cordelia <design.angelova@gmail.com>
2014-09-19 12:03:22 +0300

    define classes

---
+++ Persons/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+public class Person
+{
+    private string name;
+    private int age;
+    private string email;
+
+    public Person(string name, int age, string email = null)
+    {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+    public Person(string name, int age) : this(name, age, null) { }
+    public string Name
+    {
+        get { return this.name; }
+        set
+        {
+            if (string.IsNullOrEmpty(this.name))
+            {
+                throw new ArgumentNullException("Empty name");
+            }
+            else
+            {
+                this.name = value;
+            }
+        }
+    }
+    public int Age
+    {
+        get { return this.age; }
+        set
+        {
+            if (value < 0 || value > 100)
+            {
+                throw new ArgumentNullException("Age is out of range");
+            }
+            else
+            {
+                this.age = value;
+            }
+        }
+    }
+    public string Email
+    {
+        get { return this.email; }
+        set
+        {
+            if (value != null && !value.Contains("@"))
+            {
+                throw new ArgumentException("Invalid email!");
+
+            }
+            else
+            {
+                this.email = value;
+            }
+        }
+    }
+    public override string ToString()
+    {
+        return string.Format("name: {0}, age: {1}", this.Name, this.Age) + (this.Email == null ? "" : ", email: " + this.Email);
+    }
+
+}
+
---
+++ Persons/Persons.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persons
+{
+    class Persons
+    {
+        static void Main(string[] args)
+        {
+           Person george = new Person("George", 35);
+           Person pesho = new Person("Pesho", 25, "@");
+           Person alex = new Person("Alex", 25, "$");
+           Person ani = new Person("Ani", 20, "design.angelova@gmail.com");
+
+           Console.WriteLine("{0} {1} {2} {3}", george, pesho,alex, ani);
+        }
+    }
+}
